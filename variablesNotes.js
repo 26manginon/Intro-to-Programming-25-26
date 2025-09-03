74a2b8c5329cf39891173471ea684c66d4678ac4
Mangino <26ManginoN@badinhs.org>
2025-09-03 14:21:08 -0400

    practiced with numbers and operators

--- variablesNotes.js
+++ variablesNotes.js
@@ -1,1 +1,48 @@
 //This is a comment. This is visible to the user, but not to the computer itself. They're very useful. 
+// console.log("Hello, world!");
+
+// let fish;
+// fish = 3;
+
+// let cats = 7;
+
+// console.log(fish);
+// console.log(cats);
+// console.log(cats + fish);
+
+// let first = 1;
+// const second = 2;
+// var third = 3;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// first = 1 + 1;
+// console.log(first); 
+
+// first = 7;
+// // second = 8;
+// third = 9;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// OPERATORS +, -, *, /, ** (exponent), % (modulo)
+// console.log(3 + 5);
+// console.log(8/ 3);
+// console.log(35 + 5 / 4);
+// console.log((35 + 5) / 4)
+
+let a = 10
+console.log(a);
+a = 8
+console.log(a);
+let b = 7 * a 
+console.log(b);
+
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)
