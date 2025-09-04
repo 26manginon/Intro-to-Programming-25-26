697c3ba5af293fa2bd8f9c5ffceffcd541208bf8
Mangino <26ManginoN@badinhs.org>
2025-09-04 14:30:13 -0400

    Make new file

--- variablesNotes.js
+++ variablesNotes.js
@@ -35,14 +35,40 @@
 // console.log(35 + 5 / 4);
 // console.log((35 + 5) / 4)
 
-let a = 10
-console.log(a);
-a = 8
-console.log(a);
-let b = 7 * a 
-console.log(b);
+// let a = 10
+// console.log(a);
+// a = 8
+// console.log(a);
+// let b = 7 * a 
+// console.log(b);
 
-const max = 57
-const actual = max - 13
-const percentage = actual / max
-console.log(percentage)
+// const max = 57
+// const actual = max - 13
+// const percentage = actual / max
+// console.log(percentage)
+
+
+let num1 = 1;
+
+//ways to change a variable
+num1 = 1 + num1; //this increases num1 by 1
+num1++; //this will increase the value of num1 by one as a shortcut 
+num1+= 2; //this will increase the value of num1 by 2
+num1 *= 2; //this will multiply num1 by 2
+num1 -= 3; //this would subtract 3 from num1
+num1 /= 4; //this will divide num1 by 4
+//thisIsCamelCase
+//this_is_snake_case
+//number is already programmed into javascript.
+
+
+
+//these are notes on data types and conditionals in javascript
+//javaScript s a dynamically typed languague. C/java/C__ are statically typed
+// in a dynamically typed language you can change what "type" of value is stored in a given variable
+
+//example:
+let message = "hello"
+message = 123;
+
+//
