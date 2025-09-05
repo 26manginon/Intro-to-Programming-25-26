fcd93995fdf855e3a48f5d7ec7a9c4a208a1a610
Mangino <26ManginoN@badinhs.org>
2025-09-05 09:50:13 -0400

    created notes on conditionals in js

--- variablesNotes.js
+++ variablesNotes.js
@@ -63,12 +63,5 @@ num1 /= 4; //this will divide num1 by 4
 
 
 
-//these are notes on data types and conditionals in javascript
-//javaScript s a dynamically typed languague. C/java/C__ are statically typed
-// in a dynamically typed language you can change what "type" of value is stored in a given variable
 
-//example:
-let message = "hello"
-message = 123;
 
-//
