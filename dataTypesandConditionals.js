e1a336fd0b86d524523ec9040a0588725aebc96f
Mangino <26ManginoN@badinhs.org>
2025-09-15 14:23:30 -0400

    finished

---
+++ dataTypesandConditionals.js
@@ -0,0 +1,7 @@
+//these are notes on data types and conditionals in javascript
+//javaScript s a dynamically typed languague. C/java/C__ are statically typed
+// in a dynamically typed language you can change what "type" of value is stored in a given variable
+
+//example:
+let message = "hello"
+message = 123;
