3d2426aafc17448b834d55831a09112bfa685e3f
Mangino <26ManginoN@badinhs.org>
2025-09-24 15:23:15 -0400

    finished practice problems

--- conditionalsPractice.js
+++ conditionalsPractice.js
@@ -90,6 +90,137 @@
 //  Functionally, javaScripts AND function finds the first "falsy" value, then returns it 
 //  If not falsy values are found it returns the last value.
 
-console.log(10 && 5 && 0 && 1);
-console.log(10 && 5 && 1);
-console.log(10 && 5 && 15);
+// console.log(10 && 5 && 0 && 1);
+// console.log(10 && 5 && 1);
+// console.log(10 && 5 && 15);
+
+//  && has higher "precedence" than || (pemdas)
+// console.log(true && false || true && false);     
+
+//  ! - NOT -   First it converts the value to a Boolean, then it flips the truthiness of the value.
+// console.log(!true);
+// console.log(0);
+// console.log(!0);
+
+//  Becuase of this converison element, you can use "!!" to convert something to a boolean value 
+// console.log(!!0);   //returns false 
+
+
+//  NOT has the highest precedence of all the logical operators 
+
+//  ? is a "conditional operator". (ternary)
+//  Example:    Traditional approach - let a person sign up for a social media site if they're over 
+//  the age of 13 
+
+//  TRADITIONAL METHOD
+// let registrationAllowed;
+// let age = prompt("How old are you in years?","");
+
+// if (age > 13){
+//     registrationAllowed = true;
+// } else {
+//     registrationAllowed = false;
+// }
+
+// console.log(registrationAllowed);
+
+//  USING CONDITIONAL OPERATOR (?)
+// let age = prompt("How old are you (in years)?","");
+// let registrationAllowed = (age > 13) ? true : false;
+// console.log(registrationAllowed);
+
+//  You can use multiple ?s. Example:
+// let age = prompt("age?", "");
+
+// let message = (age < 3) ? "You're just a baby!" :
+//     (age < 10) ? "Hi there" :
+//     (age < 60) ? "Hello" :
+//     (age < 100) ? "Howdy, Grandpa" :
+//     "Dang. you're OLD."; 
+
+// console.log(message);
+
+//  SWITCHES - a swtich is a different way to do an if/else if/else if... chain 
+//  switches test for "Strict Equality"/ "===" is hwo you test for strict equality 
+//  Strict equality is when the values are the same AND the datatypes match.
+
+// console.log(3 == "3");          //This evaluates as true
+// console.log(3 === "3");         //This evaluates as false
+
+//  Example Switch:
+// let a = 2 + 2
+
+// switch (a) {
+//     case 3:
+//         alert("Too Small");
+//         break;
+//     case 4: 
+//         alert("Exactly right!");
+//         break;
+//     case 5:
+//         alert("Too Big");
+//         break;
+//     default: 
+//         alert("What are you even doing?");
+// }
+
+
+// FOR THE FIRST 5 QUESTIONS, TRY TO ANSWER BEFORE TESTING THE OUTPUT
+// 1.  what will this do: alert( null || 2 || undefined );
+// 2.  what will this do: alert( alert(1) || 2 || alert(3) );
+// 3.  what will this do: alert( 1 && null && 2 );
+// 4.  what will this do: alert( null || 2 && 3 || 4 );
+// 5.  what will this do:  console.log(console.log(1) && console.log(2));
+
+// 6.  Write an if condition to check that age is between 14 and 90 inclusively.  (“Inclusively” means that age can reach the edges 14 or 90.)
+// 7.  Write an if condition to check that age is NOT between 14 and 90 inclusively.
+// Create two variants: the first one using NOT !, the second one – without it.
+// 8.  Which of these alerts are going to execute?  What will the results of the expressions be inside if(...)?:
+//      -  if (-1 || 0) alert( 'first' ); 
+//      -  if (-1 && 0) alert( 'second' ); 
+//      -  if (null || -1 && 1) alert( 'third' );
+
+//  1.  It will yeild 2, since it is testing for any value that is true, and null and underfinded
+//      are false 
+//  2.  It will alert 1 first becuase it's the first truthy value, then it will move onto alert 2
+//      since it's the next truthy value
+//  3.  Null since it's using && all the values are not true 
+//  4.  3 since its the defaulted to the last value since not falsy values were found
+//  5.  It will console log the number 1 and then undefined because 
+
+//  6.  
+if (age >= 14 && age <= 90){
+    console.log("You're of age");
+} else {
+    console.log("Not required age");
+}
+
+//  7a.
+if (!(age >= 14 && age <= 90)){
+    console.log("Not requred age");
+}
+
+//  7b. 
+if (age < 14 || age > 90) {
+    console.log("Not required age");
+}
+   
+// //  6.  7.
+let age = prompt("Enter Age Here")
+if (age > 13 && age < 91){
+    console.log("Access Granted");
+} else {
+    console.log("Not of Age");
+}
+
+//  8.  
+//      The first and the third will execute 
+//  Result of #1
+//      Returns the first truthy value which was -1
+//  Result of #3
+//      Returns the first truthy value which was 1 
+
+
+
+
+
