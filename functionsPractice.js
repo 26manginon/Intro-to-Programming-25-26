a5314f3e566ad7949be610f28b7c90153718f336
Mangino <26ManginoN@badinhs.org>
2025-10-06 14:25:57 -0400

    practiced functions

---
+++ functionsPractice.js
@@ -0,0 +1,23 @@
+//  1.
+
+// let multiply =(a,b) => a * b; 
+// console.log(multiply(5,10));
+
+//  4.
+//      No, there is not a difference because the true 
+
+//  5.
+// function checkAge(age) { if (age > 18) { return true; } else { return confirm('Did parents allow you?');}}
+
+//  6.
+// function min(a , b){
+//     if (a < b){
+//         console.log(a);
+//     return ("A is greater than B");
+// } else {
+//     console.log(b);
+//     return ("B is greater than A");
+// }
+// }
+
+//  7.
