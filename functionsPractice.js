673741f4665b1df22439cd333fc32f2758dfe4d0
Mangino <26ManginoN@badinhs.org>
2025-10-07 13:17:32 -0400

    finsihed notes

--- functionsPractice.js
+++ functionsPractice.js
@@ -4,10 +4,11 @@
 // console.log(multiply(5,10));
 
 //  4.
-//      No, there is not a difference because the true 
+//      No, there is not a difference because and the else did not behave differently
 
 //  5.
-// function checkAge(age) { if (age > 18) { return true; } else { return confirm('Did parents allow you?');}}
+// function checkAge(age)  { if (age > 18)  { return true; } else { return confirm('Did parents allow you?');}}
+// checkAge(100)
 
 //  6.
 // function min(a , b){
@@ -21,3 +22,6 @@
 // }
 
 //  7.
+// let pow = (x,n) => x ** n;
+
+// console.log(pow(3,3));
